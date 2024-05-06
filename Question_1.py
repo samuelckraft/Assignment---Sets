a37a8e2e0be3f1130d393aadecfbaca7aefbb640
samuelckraft <samuelckraft@gmail.com>
2024-05-06 11:59:42 -0400

    update

--- Question_1.py
+++ Question_1.py
@@ -1,1 +1,10 @@
 #Task 1
+
+our_routes = {"LAX", "JFK", "CDG", "DXB"}
+competitor_routes = {"JFK", "CDG", "LHR", "BKK"}
+
+crossover_destinations = our_routes.intersection(competitor_routes) #output: {'BKK', 'LHR', 'CDG', 'JFK'}
+
+unique_destinations = our_routes.difference(competitor_routes) #output: {'LAX', 'DXB'}
+
+unshared_destinations = our_routes.symmetric_difference(competitor_routes) #output: {'BKK', 'DXB', 'LAX', 'LHR'}
